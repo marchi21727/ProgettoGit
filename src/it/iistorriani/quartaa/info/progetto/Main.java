87b86e862ab336bfdf09df7be2de6c46b3733a91
Satoscio <satoscio65@gmail.com>
2021-02-08 20:06:56 +0100

    Update Main.java
    Aggiunto un @SuppressWarnings nel Main su consiglio di Eclipse

--- src/it/iistorriani/quartaa/info/progetto/Main.java
+++ src/it/iistorriani/quartaa/info/progetto/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        @SuppressWarnings("resource") // per evitare avvertimenti di eclipse nella riga successiva
         Scanner scan = new Scanner(System.in);
         int risp = 1;
         System.out.println("Benvenuto!\n");
